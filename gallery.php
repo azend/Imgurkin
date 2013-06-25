c586128680e950da2e99bb2937f0a9c7c33f4a7b
Verdi R-D <verdi@azend.org>
2013-06-25 12:19:22 -0400

    Fixed use of action variable to determine page status

--- gallery.php
+++ gallery.php
@@ -5,9 +5,15 @@ $status = '';
 
 function rebuild_cache () {
 	global $status;
-	$status = $status . "Rebuilding cache.";
-	$_SESSION['gallery'] = json_decode( file_get_contents('http://imgur.com/gallery.json'), true );
-	$_SESSION['gallery'] = array_reverse( $_SESSION['gallery']['gallery'] );
+	$status = $status . "<h1>Rebuilding cache.</h1>";
+
+	// Grab a copy of the gallery from imgur
+	$galleryResponseRaw = file_get_contents('http://imgur.com/gallery.json');
+	$galleryResponse = json_decode( $galleryResponseRaw, true );
+
+	$gallery = $galleryResponse['data'];
+
+	$_SESSION['gallery'] = $gallery;
 	$_SESSION['gallery_pointer'] = 0;
 }
 
@@ -21,31 +27,33 @@ else {
 }
 
 
-$action = stripslashes($_GET['action']);
+if ( isset($_GET['action']) && !empty($_GET['action']) ) {
+	$action = stripslashes($_GET['action']);
 
-if ($action == "next") {
-	$status = $status . "Next.";
-	if ($_SESSION['gallery_pointer'] === count($_SESSION['gallery']) - 1) {
-		$_SESSION['gallery_pointer'] = 0;
+	if ($action == "next") {
+		$status = $status . "Next.";
+		if ($_SESSION['gallery_pointer'] === count($_SESSION['gallery']) - 1) {
+			$_SESSION['gallery_pointer'] = 0;
+		}
+		else {
+			$_SESSION['gallery_pointer'] ++;
+		}
+		header('Location: ' . strtok( $_SERVER['REQUEST_URI'], '?' ) , TRUE, 307);
 	}
-	else {
-		$_SESSION['gallery_pointer'] ++;
+	else if ($action == "prev") {
+		$status = $status . "Prev.";
+		if ($_SESSION['gallery_pointer'] === 0) {
+			$_SESSION['gallery_pointer'] = count($_SESSION['gallery']) - 1;
+		}
+		else {
+			$_SESSION['gallery_pointer'] --;
+		}
+		header('Location: ' . strtok( $_SERVER['REQUEST_URI'], '?' ) , TRUE, 307);
 	}
-	header('Location: ' . strtok( $_SERVER['REQUEST_URI'], '?' ) , TRUE, 307);
-}
-else if ($action == "prev") {
-	$status = $status . "Prev.";
-	if ($_SESSION['gallery_pointer'] === 0) {
-		$_SESSION['gallery_pointer'] = count($_SESSION['gallery']) - 1;
+	else if ($action == "rebuild") {
+		$status = $status . "Rebuild.";
+		rebuild_cache();
 	}
-	else {
-		$_SESSION['gallery_pointer'] --;
-	}
-	header('Location: ' . strtok( $_SERVER['REQUEST_URI'], '?' ) , TRUE, 307);
-}
-else if ($action == "rebuild") {
-	$status = $status . "Rebuild.";
-	rebuild_cache();
 }
 else {
 
@@ -60,14 +68,21 @@ else {
 	<html lang="en">
 		<head>
 			<meta charset="utf-8">
-			<link rel="stylesheet" href="style.css">
+			<link rel="stylesheet" href="css/normalize.css">
+			<link rel="stylesheet" href="css/style.css">
 		</head>
 		<body>
-			<h1 id="title"><?php echo $title; ?></h1>
-			<a id="prev" class="nav" href="?action=prev">Prev</a>
-			<a id="next" class="nav" href="?action=next">Next</a>
+			<div id="header" class="header">
+				<h1 id="title" class="headerItem"><?php echo $title; ?></h1>
+				<div id="nav" class="headerItem">
+					<a id="prev" class="nav" href="?action=prev">Prev</a>
+					<a id="next" class="nav" href="?action=next">Next</a>
+				</div>
+			</div>
 			<img id="image" src="<?php echo $image_url; ?>" >
-			<a id="rebuild" class="nav" href="?action=rebuild">Rebuild Cache</a>
+			<div class="footer">
+				<a id="rebuild" class="nav" href="?action=rebuild">Rebuild Cache</a>
+			</div>
 		</body>
 		<script type="text/javascript">
 			(function() {
